d6584d3a6f125694d082b439733b28148f8564b0
David Schmidt <161520441+Schmidtdtd@users.noreply.github.com>
2024-02-28 00:38:17 +0400

    Create sorting.dart

---
+++ sorting.dart
@@ -0,0 +1,14 @@
+  for (int i = lastNonZeroFoundAt; i < nums.length; ++i) {
+    nums[i] = 0;
+  }
+  return nums;
+
+    return expect(moveZeroes([1, 2, 2, 3, 5]), [1, 2, 2, 3, 5]);
+  });
+  test()
+
+  while (end >= start) {
+    if (array[start].abs() > array[end].abs()) {
+      answer[sortedIndex] = array[start] * array[start];
+      start += 1;
+    }
